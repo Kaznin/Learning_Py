f9ee7d2982f6efc23dc05ad323007c6550f3bf58
Kaznin <nakaznin@yandex.ru>
2021-12-13 00:37:03 +0300

    Под новую задачу

--- For_test.py
+++ For_test.py
@@ -1,12 +1,12 @@
-import calendar
-import datetime
+objects = [1, 2, 1, 2, 3]
+new_list = []
 
-print(calendar.monthrange(1989, 2)) #возвращает двойной кортеж c первым рабочем днем месяца и количество дней в месяце для указанного года year и месяца month.
-print(len(calendar.month(1989, 2)))
+ans = 0
 
-user_date = '02.02.89'
-user_date = datetime.datetime.strptime(user_date, '%d.%m.%y')
-print(user_date.month)
+for obj in objects: # доступная переменная objects
+    if obj not in new_list:
+        new_list.append(obj)
+    for obj
 
-delta_time = datetime.datetime(2001, 3, 3) - datetime.datetime(2000, 3, 23)
-print(delta_time)
+print(new_list, objects)
+
